0909bd3ef548534eaba5289b0176fe8c8a4c037f
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-14 01:13:05 +0600

    Blogs all content added dynamically

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,41 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
+    const {
+        title,
+        cover,
+        author_img,
+        author,
+        reading_time,
+        posted_date,
+        hashtags,
+    } = blog;
     return (
         <div>
-            
+            <img src={cover} alt="" />
+            <div className="author-container flex justify-between items-center py-3">
+                <div className="flex">
+                    <img className="w-14" src={author_img} alt="" />
+                    <div className="ml-6">
+                        <h3>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>{reading_time} min read</p>
+                </div>
+            </div>
+            <h2 className="text-4xl font-bold">{title}</h2>
+            {hashtags.map((hash, idx) => (
+                <p className="inline-block mb-10" key={idx}>
+                    #{hash}
+                </p>
+            ))}
         </div>
     );
 };
 
-
-Blog.propTypes={
-    blog: PropTypes.object.isRequired
-}
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+};
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,20 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
-    const[blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
 
-    useEffect(()=>{
-        fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    },[])
-    return (
-        <div>
-            <h2 className="mt-10 text-3xl font-semibold md:w-2/3">Blogs:{blogs.length}</h2>
-        </div>
-    );
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  return (
+    <div>
+      <h2 className="mt-10 mb-10 text-center text-3xl font-semibold md:w-2/3">
+        Blogs:{blogs.length}
+      </h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
+    </div>
+  );
 };
 
 export default Blogs;
